81cd894a3dce0836f3ff29c94c228516345edf89
Patrick <patrickegp@gmail.com>
2025-05-18 18:34:10 -0400

    Add debounce method to prevent continuous AJAX requests

--- simple-autocomplete/simple-autocomplete.js
+++ simple-autocomplete/simple-autocomplete.js
@@ -4,21 +4,20 @@
       resultClass: 'results',
       itemClass: 'item',
       minLength: 1,
+      debounceTime: 300,
       onSelect: null,
     }, options);
 
     return this.each(function() {
-
       const input     = $(this);
       const url       = input.data('url');
       const valueKey  = input.data('value');
       const labelKey  = input.data('label');
       const searchBox = input.closest('.search-box');
+      const resultsContainer = searchBox.find('.' + settings.resultClass);
 
-      input.on('keyup', function(e) {
+      const debouncedSearch = debounce(function(e) {
         const query = e.target.value;
-        const resultsContainer = searchBox.find('.' + settings.resultClass);
-
         input.prev('input').val('');
 
         if (query.length < settings.minLength) {
@@ -26,18 +25,19 @@
           searchBox.removeClass('show');
           return;
         }
+
         $.ajax({
           url: url,
           type: 'GET',
           dataType: 'json',
           data: { s: query }
         }).done(function(res) {
-          let items = res.map( (item) => 
+          let items = res.map(item => 
             `<a href="#" 
                 data-value="${item[valueKey]}" 
                 data-object='${JSON.stringify(item)}' 
-                class="${settings.itemClass}">${item[labelKey]}
-            </a>`);
+                class="${settings.itemClass}">${item[labelKey]}</a>`
+          );
 
           resultsContainer.html(items);
           if (res.length > 0) {
@@ -50,17 +50,19 @@
           resultsContainer.empty();
           searchBox.removeClass('show');
         });
-      });
+      }, settings.debounceTime);
 
-      input.on('focusout', function(e){
+      input.on('keyup', debouncedSearch);
+
+      input.on('focusout', function() {
         setTimeout(function(){
           var selectedValue = input.prev('input').val();
-          if(selectedValue.length === 0){
+          if (selectedValue.length === 0) {
             input.val('');
             searchBox.removeClass('show');
           }
         }, 200);
-      })
+      });
 
       searchBox.on('click', '.' + settings.itemClass, function(e) {
         e.preventDefault();
@@ -76,6 +78,13 @@
         }
       });
 
+      function debounce(fn, delay) {
+        let timeoutId;
+        return function (...args) {
+          clearTimeout(timeoutId);
+          timeoutId = setTimeout(() => fn.apply(this, args), delay);
+        };
+      }
     });
   };
 })(jQuery);
